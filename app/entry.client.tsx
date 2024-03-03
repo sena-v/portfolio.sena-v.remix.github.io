6e5222eb92f61b66ee748c3abd774496ab301fa2
sena-v <sena-v@users.noreply.github.com>
2024-03-03 09:35:44 +0000

    deploy: 5d1bd665dd48be2f30931cf3fe9b7d774c4c8721

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,7 @@
-import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=3125dc81"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
-import { RemixBrowser } from "/node_modules/.vite/deps/@remix-run_react.js?v=3125dc81";
-import __vite__cjsImport2_react from "/node_modules/.vite/deps/react.js?v=3125dc81"; const startTransition = __vite__cjsImport2_react["startTransition"]; const StrictMode = __vite__cjsImport2_react["StrictMode"];
-import __vite__cjsImport3_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=3125dc81"; const hydrateRoot = __vite__cjsImport3_reactDom_client["hydrateRoot"];
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=49b14fe9"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import { RemixBrowser } from "/node_modules/.vite/deps/@remix-run_react.js?v=49b14fe9";
+import __vite__cjsImport2_react from "/node_modules/.vite/deps/react.js?v=49b14fe9"; const startTransition = __vite__cjsImport2_react["startTransition"]; const StrictMode = __vite__cjsImport2_react["StrictMode"];
+import __vite__cjsImport3_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=49b14fe9"; const hydrateRoot = __vite__cjsImport3_reactDom_client["hydrateRoot"];
 startTransition(() => {
   hydrateRoot(document, /* @__PURE__ */jsxDEV(StrictMode, {
     children: /* @__PURE__ */jsxDEV(RemixBrowser, {}, void 0, false, {
